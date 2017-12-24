d230816f77428ddc55cb30260f8aca55413930a3
Firuze <pfiruza.p202@code.edu.az>
2017-12-24 19:26:58 +0400

    ajaxLab

---
+++ Ajax/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ajax.Models;
+
+
+namespace Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        AjaxEntities1 db = new AjaxEntities1();
+
+        public ActionResult Index()
+        {
+
+            ViewBag.marka = db.Marka.ToList();
+            return View(); 
+
+        }
+
+        public ActionResult getModel(int id)
+        {
+            string result = "";
+            List<Madel> models = db.Madel.Where(a => a.marka_id == id).ToList();
+
+            foreach(Madel item in models)
+            {
+                result += "<option>" + item.name + "</option>";
+            }
+            return Content(result);
+        }
+
+        public ActionResult getJson(int id)
+        {
+            var list = db.Madel.Where(a => a.marka_id == id).Select(m=> new { 
+                m.id,
+                m.name
+            }).ToList();
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Ajax/Scripts/Main.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+
+        $("select[name=marka]").change(function () {
+            var markaId = $(this).val();
+            var modelUrl = "http://localhost:51569/home/getJson/" + markaId;
+            //console.log(modelUrl);
+            $("select[name=madel]").empty();
+            $.ajax({
+                url: modelUrl,
+                type: 'get',
+                //dataType: 'html',
+                dataType:"JSON",
+                success: function (response) {
+                    //console.log("data" + response);
+                    //$("select[name=madel]").append(response);
+                    
+                    for (var i = 0; i < response.length; i++) {
+                        $("select[name=madel]").append("<option value='" + response[i].id + "'>" + response[i].name + "</option>");
+                    }
+                }
+            });
+        });
+
+
+    $("select[name=cities]").change(function () {
+        var city = $(this).val();
+        var prayUrl = "http://api.aladhan.com/timingsByCity?city=" + city + "&country=AZ";
+        //console.log(prayUrl);
+        $.ajax({
+            url: prayUrl,
+            type: 'get',
+            dataType: 'JSON',
+            success: function (response) {
+                //console.log(response.data.timings.Asr);
+                $(".timing td").eq(0).text(response.data.timings.Fajr);
+                $(".timing td").eq(1).text(response.data.timings.Dhuhr);
+                $(".timing td").eq(2).text(response.data.timings.Asr);
+                $(".timing td").eq(3).text(response.data.timings.Maghrib);
+                $(".timing td").eq(4).text(response.data.timings.Isha);
+            }
+
+        });
+    }); 
+
+  
+});
